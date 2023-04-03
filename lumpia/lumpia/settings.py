8f125fc2eab022c28a3d159953bdb65904825b86
robyn-reyes <robyn.reyes@obf.ateneo.edu>
2023-04-04 02:42:55 +0800

    discrepancy check working

--- lumpia/lumpia/settings.py
+++ lumpia/lumpia/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'lumpia.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lumpiadb1',
+        'NAME': 'lumpiadb',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
